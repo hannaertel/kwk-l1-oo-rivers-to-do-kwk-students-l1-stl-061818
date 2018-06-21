ecba0e9bfae8f82ab071b888e85995ecf3bd819b
hannaertel <hannaertel@yahoo.com>
2018-06-21 21:28:06 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -1,1 +1,14 @@
 # river.rb
+class River
+  
+  attr_accessor :name, :length, :countries, :discharge
+  def initialize(name, length, countries, discharge)
+    @name = name
+    @length = length
+    @countries = countries
+    @discharge = discharge
+  end 
+  
+end
+
+river1 = River.new("Nile", "5000", "nigeria, liberia, kenya", ) 
